85226c3c6814257e45d601ee3e77021729b6380f
KhaN <KhaN@DESKTOP-FGEGNDT>
2023-12-31 22:30:29 +0500

    Add project files.

---
+++ Blogpost/Models/Blogpost.cs
@@ -0,0 +1,10 @@
+﻿namespace Blogpost.Models
+{
+    public class Blogpost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
